4e7ad7b7566e53305d54ef0fa2a27450b51a4502
Anam Nizami <anamnizami@Anams-MBP.fritz.box>
2023-09-15 15:02:47 +0200

    different file

--- src/test/java/com/test/AndroidTest.java
+++ src/test/java/com/test/AndroidTest.java
@@ -20,7 +20,7 @@ class AndroidTest {
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
         options.setDeviceName("Nexus 6");
         options.setCapability("newCommandTimeout", 10000);
-        options.setApp(System.getProperty("user.dir")+"/apps/galaxy-QualitrainRelease-3.13.10644.283.apk");
+        options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
         AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723"),options);
         Thread.sleep(10000);
 
@@ -30,8 +30,8 @@ class AndroidTest {
 //            String elementText = element.getText();
 
             // Log the text to the terminal
-            System.out.println("Element Text: " + driver.findElement(AppiumBy.id("com.qualitrain.fitness:id/start_sign_in_button")).getText());
-            Assert.assertEquals(driver.findElement(AppiumBy.id("com.qualitrain.fitness:id/start_sign_in_button")).getText(),"Log In");
+//            System.out.println("Element Text: " + driver.findElement(By.xpath("com.qualitrain.fitness:id/start_sign_in_button")).getText());
+            Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.TextView[@content-desc=\"store item text\"])[1]")).getText(),"Sauce Labs Backpack");
         } finally {
             driver.quit();
         }
