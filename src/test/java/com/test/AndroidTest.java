61538adca1d70ed6f0a270a5b8af54cd043c9e2e
anamnizami <118906364+anamnizami@users.noreply.github.com>
2023-09-16 17:32:01 +0200

    Update AndroidTest.java

--- src/test/java/com/test/AndroidTest.java
+++ src/test/java/com/test/AndroidTest.java
@@ -13,30 +13,30 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 class AndroidTest {
-//     @Test
-//      void AndroidLaunchTest () throws InterruptedException, MalformedURLException {
-//         UiAutomator2Options options = new UiAutomator2Options();
-//         options.setPlatformName("Android");
-//         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-//         options.setDeviceName("Nexus 6");
-//         options.setCapability("newCommandTimeout", 30000);
-//         options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
-//         AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723"),options);
-//         Thread.sleep(4000);
+    @Test
+     void AndroidLaunchTest () throws InterruptedException, MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setDeviceName("Nexus 6");
+        options.setCapability("newCommandTimeout", 30000);
+        options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
+        AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723"),options);
+        Thread.sleep(4000);
 
-//         try {
+        try {
 
 
-// //            String elementText = element.getText();
+//            String elementText = element.getText();
 
-//             // Log the text to the terminal
-// //            System.out.println("Element Text: " + driver.findElement(By.xpath("com.qualitrain.fitness:id/start_sign_in_button")).getText());
-//             Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.TextView[@content-desc=\"store item text\"])[1]")).getText(),"Sauce Labs Backpack");
-//         } finally {
-//             driver.quit();
-//         }
+            // Log the text to the terminal
+//            System.out.println("Element Text: " + driver.findElement(By.xpath("com.qualitrain.fitness:id/start_sign_in_button")).getText());
+            Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.TextView[@content-desc=\"store item text\"])[1]")).getText(),"Sauce Labs Backpack");
+        } finally {
+            driver.quit();
+        }
 
-//     }
+    }
 
     
     @Test
