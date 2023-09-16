4d76e8016e087e7a3d33c36284366b1d627fb2a2
anamnizami <118906364+anamnizami@users.noreply.github.com>
2023-09-16 19:01:54 +0200

    Update AndroidTest.java

--- src/test/java/com/test/AndroidTest.java
+++ src/test/java/com/test/AndroidTest.java
@@ -8,21 +8,22 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.testng.Assert;
-
+import java.util.logging.Logger;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 class AndroidTest {
+    private static final Logger logger = Logger.getLogger(AndroidTest.class.getName());
     @Test
      void AndroidLaunchTest () throws InterruptedException, MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setDeviceName("Nexus 6");
+//        options.setDeviceName("Neiuoi");
         options.setCapability("newCommandTimeout", 30000);
         options.setApp(System.getProperty("user.dir")+"/apps/Android-MyDemoAppRN.1.3.0.build-244.apk");
         AndroidDriver driver = new AndroidDriver(new URL("http://localhost:4723"),options);
-        Thread.sleep(4000);
+        Thread.sleep(10000);
 
         try {
 
@@ -31,6 +32,8 @@ class AndroidTest {
 
             // Log the text to the terminal
 //            System.out.println("Element Text: " + driver.findElement(By.xpath("com.qualitrain.fitness:id/start_sign_in_button")).getText());
+            logger.warning(driver.findElement(By.xpath("(//android.widget.TextView[@content-desc=\"store item text\"])[1]")).getText());
+
             Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.TextView[@content-desc=\"store item text\"])[1]")).getText(),"Sauce Labs Backpack");
         } finally {
             driver.quit();
@@ -38,9 +41,10 @@ class AndroidTest {
 
     }
 
-    
     @Test
     void AT () {
-        assert 1 == 1;
+        logger.warning("hello hello hello hello");
+            assert 1 == 1;
     }
 }
+
