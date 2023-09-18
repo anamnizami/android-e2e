a4e5ec7eba381efd29757127b4641df6e3304388
anamnizami <118906364+anamnizami@users.noreply.github.com>
2023-09-18 16:15:26 +0200

    Update RunAppiumServer.sh

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,6 +1,6 @@
 set -ex
 npm install -g appium@next
 appium driver install uiautomator2
-appium plugin install element-wait
+appium plugin install --source=npm appium-wait-plugin
 appium -v
 appium --use-plugin=element-wait --log appium.log &>/dev/null &
